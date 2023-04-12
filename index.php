09104853b693e9d5174eaa3c55e484a773e8872e
Japhet klo <110985868+japhetklo@users.noreply.github.com>
2023-04-12 16:16:13 +0200

    k

--- index.php
+++ index.php
@@ -1,35 +1,67 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="UTF-8">
-    <title>TP test Math</title>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-  </head>
-  <body>
-    <div id="sidebar">
-      <ul>
-        <li><a href="#Addition" onclick="showContent('Addition')">Addition de deux nombres</a></li>
-        <li><a href="#PGN" onclick="showContent('PGN')">Les plus grand nombre</a></li>
-        <li><a href="#traduction" onclick="showContent('traduction')">Traduction Jours</a></li>
-      </ul>
-    </div>
-    <div id="content">
-      <h2>Contenu central</h2>
-      <p>Sélectionnez un menu sur la gauche pour afficher son contenu ici.</p>
-    </div>
-    
-   <script>
-    function showContent(menu) {
-    var content = document.getElementById("content");
-    if (menu == "Addition") {
-      content.innerHTML = "<h2>Addition</h2><p></p>";
-    } else if (menu == "PGN") {
-      content.innerHTML = "<h2>Plus grand nombre</h2><p>Contenu du menu 2</p>";
-    } else if (menu == "traduction") {
-      content.innerHTML = "<h2>Traduire les jours de la semaine</h2><p>Contenu du menu 3</p>";
-    }
-  }
-   </script>
-  </body>
-</html>
+<head>
+	<title>Mon projet</title>
+	<style>
+		/* styles pour le menu latéral */
+		.menu {
+			float: left;
+			width: 20%;
+			background-color: #f2f2f2;
+			height: 100%;
+			position: fixed;
+			overflow-y: auto;
+		}
+		/* styles pour le contenu centré */
+		.content {
+			margin-left: 20%;
+			padding: 20px;
+		}
+	</style>
+</head>
+<body>
 
+	<div class="menu">
+		<ul>
+			<li><a href="?action=home">Accueil</a></li>
+			<li><a href="?action=addition">Additionner</a></li>
+		</ul>
+	</div>
+
+	<div class="content">
+		<?php
+		// Vérifiez quelle action a été demandée
+		if(isset($_GET['action'])) {
+			$action = $_GET['action'];
+		} else {
+			$action = 'home';
+		}
+
+		// Chargez le contenu approprié en fonction de l'action
+		switch($action) {
+			case 'addition':
+				if(isset($_POST['num1']) && isset($_POST['num2'])) {
+					// Si des valeurs ont été soumises, effectuez l'addition et affichez le résultat
+					$num1 = $_POST['num1'];
+					$num2 = $_POST['num2'];
+					$result = $num1 + $num2;
+					echo "<h2>Résultat: $result</h2>";
+				} else {
+					// Sinon, affichez le formulaire d'addition
+					echo '<h2>Additionner</h2>';
+					echo '<form method="post">';
+					echo 'Nombre 1: <input type="text" name="num1"><br>';
+					echo 'Nombre 2: <input type="text" name="num2"><br>';
+					echo '<input type="submit" value="Additionner">';
+					echo '</form>';
+				}
+				break;
+			default:
+				echo '<h2>Accueil</h2>';
+				echo '<p>Bienvenue sur la page d\'accueil de mon projet.</p>';
+		}
+		?>
+	</div>
+
+</body>
+</html>
